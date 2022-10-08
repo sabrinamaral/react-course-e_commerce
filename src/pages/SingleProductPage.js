f705d800e7cb7f18c1d92587a255601443c03cd1
sabrinamaral <sabrinamaral@hotmail.com>
2022-10-07 21:13:30 -0300

    router6 navigate

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
-import { useParams, useHistory } from 'react-router-dom'
-import { useProductsContext } from '../context/products_context'
-import { single_product_url as url } from '../utils/constants'
-import { formatPrice } from '../utils/helpers'
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useProductsContext } from "../context/products_context";
+import { single_product_url as url } from "../utils/constants";
+import { formatPrice } from "../utils/helpers";
 import {
   Loading,
   Error,
@@ -10,13 +10,17 @@ import {
   AddToCart,
   Stars,
   PageHero,
-} from '../components'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+} from "../components";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+// ROUTER 6
+// const navigate = useNavigate()
+// inside useEffect() >> setTimeout() >> instead of history.push('/') change for navigate('/')
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
-}
+  return <h4>single product page</h4>;
+};
 
 const Wrapper = styled.main`
   .product-center {
@@ -50,6 +54,6 @@ const Wrapper = styled.main`
       font-size: 1.25rem;
     }
   }
-`
+`;
 
-export default SingleProductPage
+export default SingleProductPage;
