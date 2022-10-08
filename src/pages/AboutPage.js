68b0189b97213b5aca6b27b625dd6d495f4df696
sabrinamaral <sabrinamaral@hotmail.com>
2022-10-08 15:50:41 -0300

    about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            vitae quia accusamus alias voluptate recusandae molestiae qui. Ea
+            adipisci, ullam illum quo eos necessitatibus? Dolorum suscipit quasi
+            modi voluptatibus placeat! Corporis ratione asperiores omnis quae
+            enim rerum eaque harum neque debitis, error ipsum incidunt assumenda
+            ipsam, praesentium maiores!
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +54,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
