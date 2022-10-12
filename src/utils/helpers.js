d7f3e390a9fc717a28f156349c4fe89cd5e6082d
sabrinamaral <sabrinamaral@hotmail.com>
2022-10-12 18:44:35 -0300

    helpers | getUniqueValues

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,4 +5,17 @@ export const formatPrice = (number) => {
   }).format(number / 100);
 };
 
-export const getUniqueValues = () => {};
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  if (type === "colors") {
+    unique = unique.flat();
+  }
+  return ["all", ...new Set(unique)];
+};
+
+const array = ["a", "d", "f", "b", "c"];
+const newArray = array.sort((a, b) => {
+  return a.localeCompare(b);
+});
+
+console.log(newArray);
