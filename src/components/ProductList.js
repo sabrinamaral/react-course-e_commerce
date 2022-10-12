27fcc45127ef11d1b1b5f27890adae01c81b2618
sabrinamaral <sabrinamaral@hotmail.com>
2022-10-11 21:29:10 -0300

    product list component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import GridView from './GridView'
-import ListView from './ListView'
+import React from "react";
+import { useFilterContext } from "../context/filter_context";
+import GridView from "./GridView";
+import ListView from "./ListView";
 
 const ProductList = () => {
-  return <h4>product list</h4>
-}
+  const { filtered_products: products, grid_view } = useFilterContext();
+  if (products.length < 1) {
+    return (
+      <h5 style={{ textTransform: "none" }}>
+        Sorry no products match your search...
+      </h5>
+    );
+  }
+  return grid_view ? (
+    <GridView products={products}></GridView>
+  ) : (
+    <ListView products={products}></ListView>
+  );
+};
 
-export default ProductList
+export default ProductList;
