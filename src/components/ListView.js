6ed568ffd33121ed76bd47998c72924d74a30b8e
sabrinamaral <sabrinamaral@hotmail.com>
2022-10-16 10:06:27 -0300

    minor adjustment

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { formatPrice } from "../utils/helpers";
 import { Link } from "react-router-dom";
-import Product from "./Product";
+
 const ListView = ({ products }) => {
   return (
     <Wrapper>
